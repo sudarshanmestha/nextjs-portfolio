256f03cee7da3bdf7940467117e9937a2c363264
sudarshanmestha <sudarshan15399@gmail.com>
2025-10-27 19:29:25 +0530

    frontpage setup

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,7 @@ export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
+    url: `${baseUrl}/Ai-Tools/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }))
 
