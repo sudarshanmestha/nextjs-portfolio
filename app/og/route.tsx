d808ff3667e9e6d1d4989a5ddb2b36496930ed25
sudarshanmestha <sudarshan15399@gmail.com>
2025-11-28 14:13:26 +0530

    updated home page

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,14 +1,22 @@
-import { ImageResponse } from 'next/og'
+import { ImageResponse } from "next/og";
+
+// Recommended for OG routes
+export const runtime = "edge";
 
 export function GET(request: Request) {
-  let url = new URL(request.url)
-  let title = url.searchParams.get('title') || 'Next.js Portfolio Starter'
+  const url = new URL(request.url);
+  const title =
+    url.searchParams.get("title") || "Next.js Portfolio Starter";
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
-        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
+      <div
+        tw="flex flex-col w-full h-full items-center justify-center bg-white"
+      >
+        <div
+          tw="flex flex-col md:flex-row w-full py-12 px-4 items-center justify-between p-8"
+        >
+          <h2 tw="text-4xl font-bold tracking-tight text-left">
             {title}
           </h2>
         </div>
@@ -18,5 +26,5 @@ export function GET(request: Request) {
       width: 1200,
       height: 630,
     }
-  )
+  );
 }
