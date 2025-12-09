688ca8753d52504e33c3f701b1529f2669bd0630
sudarshanmestha <sudarshan15399@gmail.com>
2025-12-09 22:34:12 +0530

    updated home page

--- app/components/BaseUrl.tsx
+++ app/components/BaseUrl.tsx
@@ -1,23 +1,14 @@
-// app/components/BaseUrl.tsx
-import { ReactNode } from 'react'
+"use client";
 
-type Props = {
-  path?: string
-  children?: ReactNode
-  className?: string
-  target?: string
-  [key: string]: any
-}
+import Link from "next/link";
 
-export default function BaseUrl({ path = '', children, className, target = '_blank', ...rest }: Props) {
-  const base = process.env.NEXT_PUBLIC_BASE_URL || 
-               (typeof window !== 'undefined' ? window.location.origin : 'https://justpython.in')
-  
-  const href = `${base}${path.startsWith('/') ? path : '/' + path}`
+export default function BaseUrl({ path, className, children }) {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "";
+  const finalUrl = `${baseUrl}${path}`;
 
   return (
-    <a href={href} target={target} rel="noopener noreferrer" className={className} {...rest}>
-      {children ?? href}
-    </a>
-  )
+    <Link href={finalUrl} className={className}>
+      {children}
+    </Link>
+  );
 }
