14e8f38cccc87413835b8a3a7b1d43437892c9b4
sudarshanmestha <sudarshan15399@gmail.com>
2025-12-09 09:22:12 +0530

    updated home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,10 +47,11 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
+        'text-black dark:!text-white bg-white dark:bg-black',
         GeistSans.variable,
         GeistMono.variable
       )}
+      
     >
       <body>
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
