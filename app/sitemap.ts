0487ef06d13d88e30e8d8f44876926bcb2841984
sudarshanmestha <sudarshan15399@gmail.com>
2026-04-22 23:09:40 +0530

    justpython

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from './Ai-Tools/utils'
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = 'https://justpython.in'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
